a239913946682ede225135f539455f874108a610
Manoel Castro <manoelcl@gmail.com>
2022-04-26 18:12:25 +0200

    generic error and helpers.js

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ const port = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
 
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.send({
+    status: "error",
+    message: error.message,
+  });
+});
+
 app.listen(port, () => {
   console.log(chalk.green(`app listening in port ${port}`));
 });
