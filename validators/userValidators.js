03009690f6a3d7754b6a94afc7a67a688f4f9eb7
Manoel Castro <manoelcl@gmail.com>
2022-04-27 16:33:36 +0200

    finished validators

--- validators/userValidators.js
+++ validators/userValidators.js
@@ -2,4 +2,35 @@ const Joi = require("joi");
 
 const { generateError } = require("../helpers");
 
-const new
+const createUserSchema = Joi.object().keys({
+  email: Joi.string()
+    .email()
+    .required()
+    .error(generateError("Email must have a valid format", 400)),
+  password: Joi.string()
+    .min(8)
+    .required()
+    .error(
+      generateError(
+        "Password must be a valid password with at least 8 characters",
+        400
+      )
+    ),
+  username: Joi.string()
+    .min(2)
+    .required()
+    .error("User name must be at least 2 characters long", 400),
+});
+
+const loginUserSchema = Joi.object().keys({
+  email: Joi.string().email().required.error(generateError("login error", 400)),
+  password: Joi.string()
+    .min(10)
+    .required()
+    .error(generateError("login error", 400)),
+});
+
+module.exports = {
+  createUserSchema,
+  loginUserSchema,
+};
